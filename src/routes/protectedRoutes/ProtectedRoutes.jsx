19d6e363784427d42e95dff25e9f41bfd879646b
unknown <spartha343@gmail.com>
2024-06-15 15:48:30 +0600

    reverts back

--- src/routes/protectedRoutes/ProtectedRoutes.jsx
+++ src/routes/protectedRoutes/ProtectedRoutes.jsx
@@ -5,10 +5,10 @@ import useAuthInfo from "../../hooks/authInfo/useAuthInfo";
 const ProtectedRoutes = ({ children }) => {
   const location = useLocation();
   const { user, isLoading } = useAuthInfo();
-  if (!user) {
-    return <Navigate to="/sign-in" state={{ from: location }} replace />;
-  } else if (isLoading) {
+  if (isLoading) {
     return <Loading />;
+  } else if (!user) {
+    return <Navigate to="/sign-in" state={{ from: location }} replace />;
   }
   return <>{children}</>;
 };
